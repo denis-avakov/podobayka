c89f128fcc955d77b8b7a2d38c318401648c09d7
Denis Avakov <denis.avakov@pm.me>
2022-07-10 16:53:10 +0300

    fix: hide bot credentials

--- packages/twitch-sirenachan/src/utils/storage.ts
+++ packages/twitch-sirenachan/src/utils/storage.ts
@@ -1,4 +1,4 @@
 import { JsonDB } from 'node-json-db';
 import { Config } from 'node-json-db/dist/lib/JsonDBConfig';
 
-export default new JsonDB(new Config('src/database.json', true));
+export default new JsonDB(new Config('database.json', true));
