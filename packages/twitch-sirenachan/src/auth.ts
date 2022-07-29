93fdc5f3bd2ed845d84a964e74a2bb604f489877
Denis Avakov <denis.avakov@pm.me>
2022-07-29 04:47:36 +0300

    refactor: remove PubSubClient

--- packages/twitch-sirenachan/src/auth.ts
+++ packages/twitch-sirenachan/src/auth.ts
@@ -1,6 +1,5 @@
 import { RefreshingAuthProvider } from '@twurple/auth';
 import { ChatClient } from '@twurple/chat';
-import { PubSubClient, PubSubRedemptionMessage } from '@twurple/pubsub';
 import storage from 'utils/storage';
 
 export default async function auth() {
@@ -16,13 +15,6 @@ export default async function auth() {
     },
     storage.getData('/token')
   );
-
-  const pubSubClient = new PubSubClient();
-  const userId = await pubSubClient.registerUserListener(authProvider);
-
-  await pubSubClient.onRedemption(userId, (message: PubSubRedemptionMessage) => {
-    console.log('_', message.rewardTitle);
-  });
 
   return new ChatClient({
     authProvider,
