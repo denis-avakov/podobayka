4924c1d245d1fd8a40cefee67771f187fb8d5a95
Denis Avakov <56247240+denis-avakov@users.noreply.github.com>
2023-06-27 02:33:34 +0300

    chore: update the build scripts

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+// Read about how to use custom configuration:
+// https://tsup.egoist.dev/#using-custom-configuration
+
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  watch: true,
+  onSuccess: 'npm start',
+  clean: true
+});
